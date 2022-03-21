3f664f70e40ff6e2bdce248c64b2301375a1ff8c
MinWG Lee <528952776@qq.com>
2022-03-21 12:28:21 +0800

    添加项目文件。

---
+++ MsgMonitor/ProcessForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+namespace MsgMonitor
+{
+    partial class ProcessForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private ListView listView1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(422, 286);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "进程Id";
+            this.columnHeader1.Width = 80;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "进程名";
+            this.columnHeader2.Width = 180;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "窗口标题";
+            this.columnHeader3.Width = 280;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "选择";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(163, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "刷新";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(299, 304);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 24);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "取消";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ProcessForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 337);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "ProcessForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "选择进程";
+            this.Load += new System.EventHandler(this.ProcessForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader3;
+    }
+}
---
+++ mtfcore/mtfcore.cpp
@@ -0,0 +1,53 @@
+﻿#include "mtfcore.hpp"
+#include "windows.h"
+
+HWND hwnd;
+
+BOOL WINAPI Hook_WriteProcessMemory(HANDLE hProcess, LPVOID lpBaseAddress, LPCVOID lpBuffer, SIZE_T nSize, SIZE_T* lpNumberOfBytesWritten)
+{
+	Suspend_HOOK();
+	if (!IsWindow(hwnd))
+		hwnd = FindWindow(NULL, L"MsgMonitor");
+	if (IsWindow(hwnd))
+	{
+		COPYDATASTRUCT data;
+		data.dwData = GetProcessId(hProcess);
+		data.cbData = nSize + 8;
+
+		byte* datas = (byte*)malloc(nSize + 8);
+
+		if (datas)
+		{
+			memcpy(datas, lpBuffer, nSize);
+
+			*(long long*)(&datas[nSize]) = (long long)lpBaseAddress;
+
+			data.lpData = (PVOID)datas;
+			SendMessage(hwnd, WM_COPYDATA, NULL, (LPARAM)&data);
+
+			free(datas);
+		}
+		
+		/*long long* buffer = (long long*)((long long)lpBuffer + nSize);
+		*buffer = (long long)lpBaseAddress;*/
+		/*data.lpData = (PVOID)lpBuffer;*/
+
+		BOOL result = WriteProcessMemory(hProcess, lpBaseAddress, lpBuffer, nSize, lpNumberOfBytesWritten);
+		Recovery_HOOK();
+		return result;
+	}
+	else
+		return WriteProcessMemory(hProcess, lpBaseAddress, lpBuffer, nSize, lpNumberOfBytesWritten);
+}
+
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+	{
+		hwnd = FindWindow(NULL, L"MsgMonitor");
+		if (hwnd)Install_HOOK(WriteProcessMemory, Hook_WriteProcessMemory);
+	}
+    return TRUE;
+}
+
